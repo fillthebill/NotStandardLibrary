e806c7edd9d0c9bbb4b3ad80156d611d6cbf6215
fillthebill <yuanxianzhipku@163.com>
2021-12-02 17:08:42 +0800

    T* as iterator; add const, add pop_back; when/where const, return by reference

--- MyVector.h
+++ MyVector.h
@@ -10,7 +10,8 @@
 using namespace std;
 
 // step2 class iterator deleted
-// step3
+// step3 const to be added?? when and where to add const??
+// whether return by reference when downsize;
 /**
  * 1. define data member as three iterators (pointers), first, last, end_of_vector;
  * 2. intuitively, start with an array of elements T and size of the array.
@@ -26,14 +27,15 @@ public:
     typedef T* iterator;
 
     MyVector(void);
-    MyVector(MyVector<T> & from); // copy constructor
-    MyVector& operator = (MyVector<T> & from);
+    MyVector(const MyVector<T> & from); // copy constructor
+    MyVector& operator = (const MyVector<T> & from);
     ~MyVector();
 
-    inline int length() {return last-first; }; // 'Redefinition of 'size' as a different kind of symbol '
+    inline int length() const {return last-first; }; // 'Redefinition of 'size' as a different kind of symbol '
     void resize();
-    void push_back(T element);
-    //T& pop_back();
+    void sizedown();
+    void push_back(const T element);
+    T& pop_back();
 
     MyVector::iterator begin();
     MyVector::iterator end();
@@ -44,43 +46,57 @@ public:
 
 private:
     iterator first;
-    iterator last;
+    iterator last; // last denotes the address of the next element.
+    // thus, when initialized, last == first; resize() is called when last == enf_of_vector;
     iterator end_of_vector;
-    void vector_init(int sz);
+
+    void vector_init(const int sz);
+    void copy_vector(const iterator from, const iterator to, int num);
 };
 
 template <typename T>
-void MyVector<T>::vector_init(int sz) {
+void MyVector<T>::vector_init(const int sz) {
     first = new T[sz];
     last = first;
     end_of_vector = first+sz;
 }
-
 
 template <typename T>
 MyVector<T>::MyVector(void) {
     vector_init(7);
 }
 
+template <typename T>
+void MyVector<T>::copy_vector(const iterator from, const iterator to, int num) {
+    iterator traverse_from = from;
+    iterator traverse_to = to;
+    for(int i = 0; i < num; ++i) {
+        *traverse_to = *traverse_from;
+        traverse_to++;
+        traverse_from++;
+    }
+}
+
 
 template <typename T>
-MyVector<T>::MyVector(MyVector<T>& from) {
-    int sz = from.size();
+MyVector<T>::MyVector(const MyVector<T>& from) {
+    int sz = from.length();
     vector_init(sz);
+    copy_vector(from.first,first, sz);
+    last = first + sz;
+    // copy the elements
 }
 
 template <typename T>
-MyVector<T>& MyVector<T>::operator= (MyVector<T>& from) {
+MyVector<T>& MyVector<T>::operator= (const MyVector<T>& from) {
     if(this == &from) {
         return *this;
     }else {
         delete[] first; // destruction of element T would be called here;
-        vector_init(from.size());
-        iterator iterator_to;
-        iterator iterator_from = from.begin();
-        for(iterator_to = first; iterator_to < end_of_vector; iterator_to++, iterator_from++) {
-            *iterator_to = *iterator_from;
-        }
+        int len = from.length();
+        vector_init(len);
+        copy_vector(from.first,first,len);
+        last = first + len;
         return *this;
     }
 }
@@ -93,27 +109,47 @@ MyVector<T>::~MyVector<T>() {
 template <typename T>
 void MyVector<T>::resize() {
     iterator old_first = first;
-    iterator old_last = last;
-    vector_init(2*this->length());
+    int old_length = this->length();
+    vector_init(2*old_length);
 
-    iterator iterator_from = old_first;
-    for(; iterator_from <= old_last; last++, iterator_from++) {
-        *last = *iterator_from;
-
-    }
+    copy_vector(old_first, first, old_length);
+    last = first + old_length;
     delete[] old_first;
 }
 
 template <typename T>
-void MyVector<T>::push_back(T element) {
+void MyVector<T>::sizedown() {
+    int len = this->length();
+    iterator old_first = first;
+    vector_init(2*len);
+    copy_vector(old_first,first,len);
+    delete[] old_first;
+    last = first + len;
+}
+
+template <typename T>
+void MyVector<T>::push_back(const T element) {
     if(last == end_of_vector) {
         resize();
         std::cout<< "resize" <<std::endl;
+        push_back(element);
     }
     else {
         *last = element;
         last ++;
     }
+}
+
+template <typename T>
+T& MyVector<T>::pop_back() {
+
+    int len = this->length();
+    if( 4 * len < end_of_vector - first) {
+        sizedown();
+        std::cout <<  "size down" << std::endl;
+    }
+    last--;
+    return *last;
 }
 
 template <typename T>
