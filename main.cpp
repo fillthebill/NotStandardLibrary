d2b3ac43082b1345352d2e6dfd7c0951d21bcd41
fillthebill <yuanxianzhipku@163.com>
2021-12-03 21:01:58 +0800

    test for vector_iterator added

--- main.cpp
+++ main.cpp
@@ -1,20 +1,21 @@
 #include <iostream>
-#include "myStringMvable.h"
-//#include "myString.h"
+#include "myString.h"
+
 #include <string>
 #include "MySharePtr.h"
 using namespace std;
 
+#include "MyVector_iterator.h"
 int main() {
 
         //cout<< "test for normal string" << endl;
-        //myString l = "hello";
-        //myString m(" world");
-        //myString n = m;
+        //myString_iterator l = "hello";
+        //myString_iterator m(" world");
+        //myString_iterator n = m;
         //n = l;
         //l.push_back(" world");
         //cout<< l <<endl;
-        
+
 
         /** cstor is called 2 times for the first 2 initiation.
          * then copy cstor is called
@@ -25,12 +26,12 @@ int main() {
 
         cout<< "test for Move aware string"<<endl;
 
-        myStringMvable s("hello");
-        myStringMvable t = " mv semantics";
-        //myStringMvable u(s+t);   /** why treat s+t as lvalue, no cstor called at all?? why??
+        myString_iteratorMvable s("hello");
+        myString_iteratorMvable t = " mv semantics";
+        //myString_iteratorMvable u(s+t);   /** why treat s+t as lvalue, no cstor called at all?? why??
         //cout<<"size of u is " << u.size()<<endl;
 
-        myStringMvable p(move(s+t));
+        myString_iteratorMvable p(move(s+t));
         // p = s+t; /** this line results in an error!
 
         int in(1000);
@@ -41,8 +42,63 @@ int main() {
 
         /**
         int* T = new int(1000);
-        MySharePtr<int> shareint(T);
+        MyShareP_iteratortr<int> shareint(T);
          */
+
+        MyVector_iterator<int> height;
+
+        for(int i = 0; i < 5; ++i) {
+            height.push_back(i);
+            std::cout<< height.length()<<std::endl;
+        }
+// MyVector_iterator<int> height2(height);
+//        std::cout<< "len of height is " << height2.length()<< std::endl;
+//
+//        MyVector_iterator<int> height3;
+//        height3 = height2;
+//        std::cout<< "len of height3 is " << height3.length()<< std::endl;
+
+
+        for(int i = 0; i < 5; ++i) {
+            int input = i+7;
+            height.push_back(input);
+            std::cout<< "end is "<< *(height.end()-1)<<std::endl;
+        }
+
+        std::sort(height.begin(),height.end());
+
+        for(int i = 0; i < 10; ++i) {
+            int temp;
+            temp = height[i];
+            std::cout<< temp << " ";
+        }
+
+        std::unique(height.begin(),height.end());
+
+        std::cout << "after unique" << std::endl;
+
+        for(int i = 0; i < height.length(); ++i) {
+            int temp;
+            temp = height[i];
+            std::cout<< temp << " ";
+        }
+
+        std::reverse(height.begin(),height.end());
+
+        std::cout << "after reverse" << std::endl;
+
+        for(int i = 0; i < height.length(); ++i) {
+            int temp;
+            temp = height[i];
+            std::cout<< temp << " ";
+        }
+
+        MyVector_iterator<int>::iterator ite = height.begin();
+        std::cout << " the first one is "<< *ite << std::endl;
+
+
+
+
 
 
 
